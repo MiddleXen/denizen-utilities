85ae0bf909050226abc5041dae16b19f08fdbf85
isnsest <gagagaopo@gmail.com>
2026-05-18 23:31:12 +0300

    chore: refine the nms initialization code

--- core/src/main/java/com/isnsest/denizenutilities/nms/NMSHandler.java
+++ core/src/main/java/com/isnsest/denizenutilities/nms/NMSHandler.java
@@ -4,6 +4,9 @@ import com.denizenscript.denizencore.utilities.debugging.Debug;
 import com.isnsest.denizenutilities.extensions.helpers.BiomeHelper;
 import org.bukkit.Bukkit;
 
+import java.util.List;
+import java.util.Optional;
+
 public abstract class NMSHandler {
 
     public static NMSHandler instance;
@@ -11,15 +14,20 @@ public abstract class NMSHandler {
     public static Object entityHelper;
     public static BiomeHelper biomeHelper;
 
+    private static final List<String> SUPPORTED_VERSIONS = List.of("1.21", "26.1");
+
+    private static Optional<String> getNMSPackageVersion(String bukkitVersion) {
+        return SUPPORTED_VERSIONS.stream()
+                .filter(bukkitVersion::contains)
+                .findFirst()
+                .map(version -> "v" + version.replace('.', '_'));
+    }
+
     public static boolean initialize() {
         String bukkitVersion = Bukkit.getBukkitVersion();
-        String packageVersion;
 
-        if (bukkitVersion.contains("26.1")) {
-            packageVersion = "v26_1";
-        } else if (bukkitVersion.contains("1.21")) {
-            packageVersion = "v1_21";
-        } else {
+        String packageVersion = getNMSPackageVersion(bukkitVersion).orElse(null);
+        if (packageVersion == null) {
             Debug.log("denizen-utilities", "This server version is not supported!");
             return false;
         }
